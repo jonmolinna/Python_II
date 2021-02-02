aa52561c2d6b7dc101e06cb7d791311a37d64bcd
jonmolinna <jmolina2624@gmail.com>
2021-02-02 08:59:40 -0500

    Proyecto Web App

---
+++ django3/proyectowebapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, "proyectowebapp/home.html")
+
+def servicios(request):
+    return render(request, "proyectowebapp/servicios.html")
+
+def tienda(request):
+    return render(request, "proyectowebapp/tienda.html")
+
+def blog(request):
+    return render(request, "proyectowebapp/blog.html")
+
+def contacto(request):
+    return render(request, "proyectowebapp/contacto.html")
