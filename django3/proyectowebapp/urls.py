55bf3a5ca3853768e1cfce9135bcabedc9fb14ac
jonmolinna <jmolina2624@gmail.com>
2021-02-09 08:52:52 -0500

    Configurando carpeta static

--- django3/proyectowebapp/urls.py
+++ django3/proyectowebapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from proyectowebapp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name="Home"),
     path('servicios', views.servicios, name="Servicios"),
@@ -8,3 +11,5 @@ urlpatterns = [
     path('blog', views.blog, name="Blog"),
     path('contacto', views.contacto, name="Contacto"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
