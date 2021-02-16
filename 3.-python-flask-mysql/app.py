a02218211e70f9b11fa2059fdf7e776bf4484607
jonmolinna <jmolina2624@gmail.com>
2021-02-16 08:41:45 -0500

    app.py

---
+++ 3.-python-flask-mysql/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask_contacts'
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname)
+        print(phone)
+        print(email)
+        return 'Recevido'
+
+# 22_55
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
