09c0e4d8309481b051ad2563efadff9c1c9cab19
jonmolinna <jmolina2624@gmail.com>
2021-02-19 09:00:56 -0500

    Funcion Eliminar

--- 3.-python-flask-mysql/app.py
+++ 3.-python-flask-mysql/app.py
@@ -12,11 +12,12 @@ mysql = MySQL(app)
 # settings
 app.secret_key = 'mysecretkey'
 
-#----------------------------- 35:58
-
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    return render_template('index.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -32,13 +33,22 @@ def add_contact():
         flash('Contact Added Successfully')
         return redirect(url_for('Index'))
 
-@app.route('/edit')
-def edit_contact():
-    return 'edit contact'
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
+    data = cur.fetchall()
+    return render_template('edit-contact.html', contact = data[0])
 
-@app.route('/delete')
-def delete_contact():
-    return 'delete contact'
+#-------------------------- 55:20
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Contact Removed Successfully')
+    return redirect(url_for('Index'))
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
