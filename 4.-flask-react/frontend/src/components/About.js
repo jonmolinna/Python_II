81f59728e12d2b97893e158cafe5a7f632319dac
jonmolinna <jmolina2624@gmail.com>
2021-03-05 07:10:46 -0500

    componentes

---
+++ 4.-flask-react/frontend/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae impedit iste, perferendis blanditiis tempore odio nulla, maiores quisquam accusamus repellat sint placeat sed veritatis dolor incidunt sequi repudiandae fugit rem consequuntur asperiores. Deserunt quas facilis, maiores voluptatem ad expedita? Ad eum quisquam eos veniam repellendus, inventore modi nemo blanditiis necessitatibus reprehenderit dolore temporibus delectus laboriosam, impedit officia mollitia adipisci? Ad ducimus architecto illum, exercitationem ratione minima atque quae sit vitae vero, sint nostrum est placeat. Cupiditate, quis. Quas incidunt velit reiciendis animi dolore voluptatem beatae, esse, voluptas, doloribus corporis eveniet praesentium. Dolore dicta aperiam blanditiis error! Neque amet quisquam libero nobis, voluptas porro ratione natus possimus quia obcaecati vel, nihil fugiat. Id repellat officia molestiae a, neque quae porro animi corporis ullam quibusdam rerum voluptates error. Ut, quaerat. Temporibus delectus vitae magni? Necessitatibus explicabo reprehenderit voluptatibus quisquam aperiam, similique minus doloremque eveniet maiores, sapiente quod! Quas dolor ipsa blanditiis ut sapiente repellendus consequuntur perspiciatis veritatis esse earum veniam, fuga repellat, obcaecati nisi doloribus provident, cum possimus vel eligendi. Id cum delectus explicabo. Culpa beatae hic ipsa quaerat nobis. Cumque temporibus esse quod quisquam aperiam quasi, consectetur accusantium, exercitationem ea officia adipisci non, dolorem sunt iusto obcaecati quas ducimus explicabo nam eum vel aut repellat ex voluptas! Quae excepturi blanditiis harum dolorum quis laboriosam tempora similique recusandae veritatis architecto non vel fugit quidem at ut a eligendi dolore assumenda earum dicta dolor doloribus, aliquid velit sunt? Repellat eaque quia impedit veritatis consectetur expedita, nemo modi cumque et illum hic praesentium eum! Doloremque dignissimos nulla quasi. Voluptas, nihil obcaecati repellendus harum architecto, consequuntur necessitatibus minus ipsum earum, odio nisi quos exercitationem tempora at quo similique quibusdam sapiente veritatis. Sed mollitia modi illo veniam, dicta in ad alias iusto excepturi magni atque velit ducimus quia officia nemo amet, vitae non tenetur exercitationem!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit enim soluta, incidunt inventore beatae dolor quisquam reprehenderit sunt quasi quod! Rerum dolore neque non quod aspernatur repellat suscipit odit doloribus alias, quisquam, incidunt praesentium animi tenetur porro est a excepturi beatae quidem molestias consequatur possimus aut officiis! Magnam quisquam eos harum voluptas illo alias vitae quidem saepe veniam impedit perspiciatis aliquid eligendi, in quae rerum magni. Architecto cupiditate iusto blanditiis numquam culpa cumque maiores veniam optio accusantium, natus voluptates! Consequuntur officia enim assumenda nemo inventore illo quam facere aliquam minima voluptates modi quo voluptatem earum illum quos, exercitationem nesciunt totam.
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ 4.-flask-react/frontend/src/components/Users.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from 'react';
+
+const API = process.env.REACT_APP_API;
+
+function Users() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [users, setUsers] = useState([]);
+    const [editing, setEditing] = useState(false);
+    const [id, setId] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        if(!editing){
+            const res = await fetch(`${API}/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password
+                })
+            })
+    
+            const data = await res.json();
+            //console.log(data);
+        } else {
+            const res = await fetch(`${API}/users/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password
+                })
+            });
+            const data = await res.json();
+            console.log(data)
+            setEditing(false);
+            setId('');
+        }
+
+        await getUsers();
+
+        setName('');
+        setEmail('');
+        setPassword('');
+    }
+
+    const getUsers = async () => {
+        const res = await fetch(`${API}/users`);
+        const data = await res.json();
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const deleteUser = async (id) => {
+        const userResponse = window.confirm('Are you sure you want to delete it?');
+        if(userResponse){
+            const res = await fetch(`${API}/users/${id}`,{
+                method: 'DELETE'
+            });
+            const data = await res.json();
+            await getUsers();
+            //console.log(data);
+        }
+    }
+
+    const editUser = async (id) => {
+        const res = await fetch(`${API}/user/${id}`);
+        const data = await res.json();
+        
+        setEditing(true);
+        setId(id);
+
+        setName(data.name);
+        setEmail(data.email);
+        setPassword(data.password);
+    }
+
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <form onSubmit={handleSubmit} className="card card-body">
+                    <div className="form-group">
+                        <input 
+                            type="text" 
+                            onChange={(e) => setName(e.target.value)} 
+                            value={name}
+                            className="form-control"
+                            placeholder="Name"
+                            autoFocus 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="email" 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            value={email}
+                            className="form-control"
+                            placeholder="Email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="password" 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            value={password}
+                            className="form-control"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <button className="btn btn-success btn-block">
+                        {editing ? 'Edit' : 'Create'}
+                    </button>
+                </form>
+            </div>
+
+            <div className="col md-6">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>Operation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => (
+                                <tr key={user._id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.password}</td>
+                                    <td>
+                                        <button className="btn btn-secondary btn-sm btn-block" onClick={() => editUser(user._id)}>
+                                            Edit
+                                        </button>
+                                        <button className="btn btn-danger btn-sm btn-block" onClick={() => deleteUser(user._id)}>
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Users;
