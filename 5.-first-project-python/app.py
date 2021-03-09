af9ae7ce742e36cdc661ccf4aa348037bf90c73b
jonmolinna <jmolina2624@gmail.com>
2021-03-09 07:18:47 -0500

    First Project Python

--- 5.-first-project-python/app.py
+++ 5.-first-project-python/app.py
@@ -1,7 +1,47 @@
 print("Welcome to my first game!")
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 print("Hello", name, "you are", age, "years old.")
 
-# 22: 29
+health = 10
+
+if age >= 18:
+    print("You are old enough!")
+
+    wants_to_play = input("Do you want to play? ").lower()
+    if wants_to_play == "yes":
+        print("You are staring with", health, "health")
+        print("Let's play!")
+
+        left_or_right = input("First choice... Left or Right (left/right)? ")
+        if left_or_right == "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ")
+            
+            if ans == "around":
+                print("You went around and reached the other side of the lake.")
+            elif ans == "across":
+                print("You managed to get across, but were bit by a fish and lost 5 health.")
+                health -= 5
+
+            ams = input("You notice a house and a river. Which do you go to (river/house)? ")
+            if ams == "house":
+                print("You go to the house and are greted by the owner... He doesn't like you and you lose 5 health")
+                health -= 5
+
+                if health <= 0:
+                    print("You now have 0 health and you lost the game...")
+                else:
+                    print("You have survived...You win!")
+
+            else:
+                print("You fell in the river and lost...")
+
+        else:
+            print("You fell down and lost...")
+
+    else:
+        print("Cya...")
+
+else:
+    print("You are not old enough to play...")
